8021e49b3acbd4c10c8771cc9143d1041d34a518
HCK42MRX <133762067+HCK42MRX@users.noreply.github.com>
2023-10-28 10:40:21 +0700

    update: file app.js connect database ATLAS

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 import express from 'express';
-import { mongoose } from './models/database.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 // Connect to the database
-mongoose
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.ATLAS_MONGODB_CONNECT_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Berhasil terkoneksi ke MongoDB');
+  } catch (err) {
+    console.error('Error: ' + err);
+  }
+};
 
 // Start the server
 app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+  connect()
+    .then(() => {
+      console.log('Server is listening on port 3000');
+    })
+    .catch((err) => {
+      console.error('Error starting the server: ' + err);
+    });
 });
