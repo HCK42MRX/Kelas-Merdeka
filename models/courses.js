32ccbc060af805ef1109fde9e94d273d9d39d9a2
HCK42MRX <buditarmijisparepart223@gmail.com>
2023-10-29 15:39:08 +0700

    update: pemindahan scheme video ke scheme course

--- models/courses.js
+++ models/courses.js
@@ -3,7 +3,7 @@ import mongoose, { Schema } from "mongoose";
 const courseScheme = new Schema({
   author: {
     type: String,
-    required: true
+    required: true,
   },
   nama_course: {
     type: String,
@@ -15,18 +15,34 @@ const courseScheme = new Schema({
     required: true,
   },
   card_video: {
-    // dummy card_video
-    type: [String],
-    required: true
+    title: {
+      type: String,
+      required: true,
+    },
+    url_video: {
+      // dummy video
+      type: [String],
+      required: true,
+    },
+    duration: {
+      type: String,
+      required: true,
+    },
+    quiz_interactive: {
+      type: Boolean,
+      default: false,
+    },
+    in_duration: [String],
+    tanggal_dibuat: {
+      type: Date,
+      default: Date.now(),
+    },
   },
   tanggal_dibuat: {
     type: Date,
-    default: Date.now()
-  }
+    default: Date.now(),
+  },
 });
-
-
-
 
 const Courses = mongoose.model("courses", courseScheme);
 
