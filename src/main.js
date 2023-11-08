c0ac4d8cd292518b65da75dd3ae7301851cb48ce
Kaoei <fahmiirfarizal@gmail.com>
2023-11-08 22:27:33 +0700

    update router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './routes/routespage'
 import './index.css'
-// import VueRouter from 'vue-router'
 
-
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
