c6eaefcca02f1058498e4140ef97424ae5fc2ce7
Kaoei <fahmiirfarizal@gmail.com>
2023-11-10 22:08:13 +0700

    update route

--- src/routes/routespage.js
+++ src/routes/routespage.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/homepage.vue'
 import Login from '../components/loginPage.vue'
 import Register from '../components/registerPage.vue'
+import courseDetail from '../components/detail_course.vue'
 
 const routes = [
  { path: '/', component: Home, meta: {header : 1, footer : 1} },
  { path: '/login', component: Login },
  { path: '/register', component: Register },
+ { path: '/detail', component: courseDetail, meta:{header : 1, footer: 1} },
 ]
 
 const router = createRouter({
